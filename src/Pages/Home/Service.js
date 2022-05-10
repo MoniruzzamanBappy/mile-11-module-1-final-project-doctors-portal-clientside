de27bb6a868284fcd4f10e328bfaebb8d3d6e88a
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-05-10 13:43:57 +0600

    footer added

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Service = ({p: items}) => {
     const {name, description, img} = items
-    console.log(items);
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -14,26 +14,26 @@ const Footer = () => {
       <div className="footer p-10">
         <div>
           <span className="footer-title">Services</span>
-          <Link className="link link-hover">Branding</Link>
-          <Link className="link link-hover">Design</Link>
-          <Link className="link link-hover">Marketing</Link>
-          <Link className="link link-hover">Advertisement</Link>
+          <Link to="/" className="link link-hover">Branding</Link>
+          <Link to="/" className="link link-hover">Design</Link>
+          <Link to="/" className="link link-hover">Marketing</Link>
+          <Link to="/" className="link link-hover">Advertisement</Link>
         </div>
         <div>
           <span className="footer-title">Company</span>
-          <Link className="link link-hover">About us</Link>
-          <Link className="link link-hover">Contact</Link>
-          <Link className="link link-hover">Jobs</Link>
-          <Link className="link link-hover">Press kit</Link>
+          <Link to="/" className="link link-hover">About us</Link>
+          <Link to="/" className="link link-hover">Contact</Link>
+          <Link to="/" className="link link-hover">Jobs</Link>
+          <Link to="/" className="link link-hover">Press kit</Link>
         </div>
         <div>
           <span className="footer-title">Legal</span>
-          <Link className="link link-hover">Terms of use</Link>
-          <Link className="link link-hover">Privacy policy</Link>
-          <Link className="link link-hover">Cookie policy</Link>
+          <Link to="/" className="link link-hover">Terms of use</Link>
+          <Link to="/" className="link link-hover">Privacy policy</Link>
+          <Link to="/" className="link link-hover">Cookie policy</Link>
         </div>
       </div>
-      <div className="mb-11 mt-32">
+      <div className="mb-11 mt-12">
         <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
       </div>
     </footer>
